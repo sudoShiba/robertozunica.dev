bc33251185d10d38c40d935f5d43e5f53f93aa69
Roberto Zunica <roberto.zunica@student.kuleuven.be>
2025-04-20 22:56:10 +0200

    made left column first item

--- src/components/StoryComponent.tsx
+++ src/components/StoryComponent.tsx
@@ -2,13 +2,16 @@ import React from "react"
 
 import { StoryComponentProps } from "../types/types.ts"
 
-const StoryComponent: React.FC<StoryComponentProps> = ({ title, content, size, image }) => {
+const StoryComponent: React.FC<StoryComponentProps> = ({ title, children, size, image, icon}) => {
   return (
     <section className={"story size-" + size}>
-      <h1>{title}</h1>
+      <div>
+        {icon && <img src={icon} className="icon" alt={title} />}
+        <h1>{title}</h1>
+      </div>
       <div>
         {image && <img src={image} alt={title} />}
-        <p>{content}</p>
+        <p>{children}</p>
       </div>
     </section>
   )
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,7 @@
 export interface StoryComponentProps {
     title: string;
-    content: string;
+    children: React.ReactNode;
     size: 1 | 2 | 3;
     image?: string;
+    icon?: string;
 }
