ceede63255c08a4b7e70c76e7ec05591fa17071d
Roberto Zunica <roberto.zunica@student.kuleuven.be>
2025-04-21 07:32:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Footer from "./components/Footer.tsx"
 import Hero from "./components/Hero.tsx"
 import SocialLinks from "./components/SocialLinks.tsx"
-import Story from "./components/Story.tsx"
 
 import "./App.css"
 
