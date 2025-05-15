2f363ffdad6655669c6f8754c24386138f5226ea
Roberto Zunica <roberto.zunica@student.kuleuven.be>
2025-05-15 14:55:08 +0200

    added links

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -54,6 +54,7 @@ const Story: React.FC = () => {
               image="https://wms.cs.kuleuven.be/cs/afbeeldingen/img-3869.jpg"
             >
               I am currently pursuing a Bachelor's degree in Computer Science at KU Leuven. My studies have provided me with a solid foundation in programming, algorithms, and software development. I am passionate about learning and applying new technologies to solve real-world problems.
+              <Site url="https://wms.cs.kuleuven.be/" />
             </StoryComponent>
             <StoryComponent
               title="High School Diploma in Maths & Sciences"
@@ -63,6 +64,7 @@ const Story: React.FC = () => {
               image="assets/SLHD.jpeg"
             >
               I graduated from high school with a focus on Maths and Sciences. This background has equipped me with strong analytical and problem-solving skills, which I apply in my studies and projects.
+              <Site url="https://sint-leo.be/" />
             </StoryComponent>
           </>
         )}
