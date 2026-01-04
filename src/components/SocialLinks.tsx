4b3584e13884f18639c593ede3836f25ec4c63cf
Roberto Zunica <roberto.zunica@student.kuleuven.be>
2026-01-04 14:46:29 +0100

    bump and edit social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,19 +1,25 @@
-import { IconBrandLinkedin, IconBrandGithub, IconBrandFacebook, IconBrandDiscord, IconMail } from "@tabler/icons-react"
+import { IconBrandLinkedin, IconBrandGithub, IconBrandFacebook, IconBrandDiscord, IconMail, IconBrandBluesky, IconBrandItch } from "@tabler/icons-react"
 import React from "react"
 
 const SocialLinks: React.FC = () => {
   return (
     <section className="social-links-wrapper">
-      <a href="https://www.linkedin.com/in/robertozunica/" target="_blank">
+      <a href="https://t.robertozunica.dev/r/linkedin" target="_blank">
         <IconBrandLinkedin stroke={2} />
       </a>
-      <a href="https://github.com/sudoShiba" target="_blank">
+      <a href="https://t.robertozunica.dev/r/github" target="_blank">
         <IconBrandGithub />
       </a>
-      <a href="https://discord.com/users/418793373365829633" target="_blank">
+      <a href="https://t.robertozunica.dev/r/discorduser" target="_blank">
         <IconBrandDiscord />
       </a>
-      <a href="https://www.facebook.com/robertozunica06/" target="_blank">
+      <a href="https://t.robertozunica.dev/r/itch">
+        <IconBrandItch />
+      </a>
+      <a href="https://t.robertozunica.dev/r/bluesky" target="_blank">
+        <IconBrandBluesky />
+      </a>
+      <a href="https://t.robertozunica.dev/r/facebook" target="_blank">
         <IconBrandFacebook />
       </a>
       <a href="mailto:me@robertozunica.dev" target="_blank">
